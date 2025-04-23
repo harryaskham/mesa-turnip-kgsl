f085647f57d9fd028ab61958061e3047892bae7b
Harry Askham <harryaskham@gmail.com>
2025-04-23 13:20:54 +0000

    kgsl

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
 { pkgs ? import <nixpkgs> {}}:
 
+# https://github.com/xDoge26/proot-setup/issues/26#issuecomment-1677977477
+
 with pkgs.lib;
 
 let
@@ -10,21 +12,19 @@ let
   pythonEnv = pkgs.python3.withPackages pythonPkgs;
 in pkgs.mkShell ({
     nativeBuildInputs = with pkgs; [
-      pythonEnv
+      pkg-config
       meson
       ninja
-      #gcc
+      pythonEnv
       clang
-      llvm
-      glib
+      clang-tools
+      llvmPackages_latest.llvm
+      llvmPackages_latest.libclang
+      llvmPackages_latest.stdenv
       libdrm
-      # stdenv.cc.cc
-      # stdenv.cc.cc.lib
-      # stdenv.cc.cc.libgcc
       zlib
       zlib.dev
       dlib
-      pkg-config
       vulkan-loader
       vulkan-headers
       vulkan-tools
@@ -51,6 +51,7 @@ in pkgs.mkShell ({
     ];
     shellHook = ''
       export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:~/.nix-profile/lib"
+      export LIBCLANG_PATH="${pkgs.llvmPackages_latest.libclang}/lib";
       export SETUP_CMD="meson build -D platforms=x11,wayland -D gallium-drivers=swrast,virgl,zink -D vulkan-drivers=freedreno -D dri3=enabled  -D egl=enabled  -D gles2=enabled -D glvnd=true -D glx=dri  -D libunwind=disabled -D osmesa=true  -D shared-glapi=enabled -D microsoft-clc=disabled  -D valgrind=disabled --prefix $PWD/build/install -D gles1=disabled -D freedreno-kgsl=true"
       export BUILD_CMD="ninja -C build/"
     '';
